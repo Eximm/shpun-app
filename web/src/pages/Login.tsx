2188335acc7688d903343598370083a4c607e9a8
Eximm <103117373+Eximm@users.noreply.github.com>
2026-04-08 20:53:09 +0200

    Update Login.tsx

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -574,7 +574,7 @@ export function Login() {
       await new Promise<void>((resolve, reject) => {
         const script = document.createElement("script");
         script.async = true;
-        script.src = "https://telegram.org/js/telegram-widget.js?22";
+        script.src = "/vendor/telegram-widget.js";
         script.setAttribute("data-telegram-login", botUsername);
         script.setAttribute("data-size", "large");
         script.setAttribute("data-userpic", "true");
